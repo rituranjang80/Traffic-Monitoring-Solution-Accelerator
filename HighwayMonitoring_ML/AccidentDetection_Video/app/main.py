cf01bcba42ac8546a476834ceb3eb4971fe9d41d
varunsaini745 <69338017+varunsaini745@users.noreply.github.com>
2022-09-07 13:20:13 +0530

    Update main.py

--- HighwayMonitoring_ML/AccidentDetection_Video/app/main.py
+++ HighwayMonitoring_ML/AccidentDetection_Video/app/main.py
@@ -12,7 +12,10 @@ async def root(file: UploadFile = File(...)):
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
         pre = predict_video(video_path=file.filename)  
-        api_url = "https://highwaymonitoringwebapi.azurewebsites.net/api/TrafficAnalysis"
+        #api_url = "https://highwaymonitoringwebapi.azurewebsites.net/api/TrafficAnalysis"
+        dir_root = os.path.dirname(os.path.abspath(__file__))
+        with open(dir_root+ "/config.yaml", "r") as yamlfile:
+            config = yaml.load(yamlfile, Loader=yaml.FullLoader)
         json_data = json.dumps(pre)
         print(json_data)
         # print(json_data)
@@ -22,5 +25,5 @@ async def root(file: UploadFile = File(...)):
             "cache-control": "no_cache"
         }
 
-        response = requests.request("POST", api_url, data=json_data, headers=headers)
+        response = requests.request("POST", config['Traffic_mon_url'], data=json_data, headers=headers)
     return "Done"
